c5f26367628c1301467dc3055f60f605244c0d65
e-Ling.Tang <e-ling.tang@geely.com>
2022-01-21 16:57:33 +0800

    feat 设置环境贴图；

--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,7 @@ import Stats from 'stats.js';
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js';
 import options from './utils';
+import { SphereGeometry } from 'three';
 
 const { Scene, PerspectiveCamera, PlaneGeometry, MeshPhongMaterial, WebGLRenderer, BoxGeometry, MeshBasicMaterial, Mesh, AxesHelper, Color, Fog } = THREE
 
@@ -32,7 +33,7 @@ scene.add( dirLight );
 // 方向光↑↑↑
 
 // ground↓↓↓
-const mesh = new THREE.Mesh( new PlaneGeometry( 100, 100 ), new MeshPhongMaterial( { color: 0x999999, depthWrite: false } ) );
+const mesh = new THREE.Mesh( new PlaneGeometry( 100, 100 ), new MeshPhongMaterial( { color: 0x999999, depthWrite: false, transparent: true, opacity: 0.7 } ) );
 mesh.rotation.x = - Math.PI / 2;
 mesh.receiveShadow = true;
 scene.add( mesh );
@@ -48,30 +49,54 @@ const loader = new GLTFLoader();
 //   });
 // } );
 
+let envtexture: any = null;
+
+const getBox = (x = 1,y = 1,z = 1) => {
+  const cube = new SphereGeometry( 0.4, 30, 30 );
+  const material = new THREE.MeshStandardMaterial({
+    // color: 0x018cff,
+    metalness: 1.0,
+    roughness: 0.6,
+    envMapIntensity: 1,
+   });
+  return new Mesh( cube, material )
+}
+
+const box = getBox()
+box.position.set(2,1,0)
+// scene.add( box );
+
+//六张图片分别是朝前的（posz）、朝后的（negz）、朝上的（posy）、朝下的（negy）、朝右的（posx）和朝左的（negx）。
+new THREE.CubeTextureLoader().setPath( 'images/g1/' ).load(['px.jpg', 'nx.jpg','py.jpg', 'ny.jpg','pz.jpg', 'nz.jpg'],
+(texture) => {
+  scene.background = texture;
+  box.material.envMap = texture;
+  envtexture = texture;
+  box.material.needsUpdate = true
+});
+
+
 let carmodel:any = null
 
-var newMaterial = new THREE.MeshStandardMaterial({color: 0xff0000});
 loader.load( 'models/gltf/spy-hypersport/scene.gltf', function ( gltf ) {
   carmodel = gltf.scene;
   console.log(carmodel)
   scene.add( carmodel );
-  carmodel.traverse( function ( object: any ) {
-    console.log(object.name)
-    if ( object.isMesh ) object.castShadow = true;
-    
+  const newMaterial = new THREE.MeshStandardMaterial({
+    // color: 0xff7c04,
+    color: 'red',
+    metalness: 1.0,
+    roughness: 0.3,
+    envMapIntensity: 1.0,
+    envMap: envtexture,
   });
+  carmodel.traverse( function ( object: any ) {
+    if ( object.isMesh ) object.castShadow = true;
+    if (object.isMesh && object.name === 'Object_22') object.material = newMaterial; // 覆盖默认材质
+  });
+  
   // https://threejs.org/examples/#webgl_loader_gltf_variants 动态切换已加载材质的示例
   // 自定义模型材质 https://threejs.org/examples/#webgl_custom_attributes   https://threejs.org/examples/#webgl_buffergeometry_custom_attributes_particles
-  setTimeout(() => {
-    carmodel.traverse( function ( object: any ) {
-      // object_2 后轮
-      // object_25 前轮
-      // object_15 前轮毂
-      // Object_22 座位
-      console.log(object.name)
-      if (object.isMesh && object.name === 'Object_22') object.material = newMaterial; // 覆盖默认材质
-    });
-  }, 5000)
 } );
 
 
@@ -100,19 +125,8 @@ document.body.appendChild( stats.dom );
 // 参数gui控制器
 console.log(options) // 在这里引用了options才会在页面显示右上角的那个框框
 
-const getBox = (x = 1,y = 1,z = 1) => {
-    const cube = new BoxGeometry( 1, 1, 1 );
-    const material = new MeshBasicMaterial( { color: 0x00ff00 } );
-    return new Mesh( cube, material )
-}
-
-const box = getBox()
-// scene.add( box );
-
 const animate = function () {
     stats.begin();
-    box.rotation.x += 0.01;
-    box.rotation.y += 0.01;
     renderer.render( scene, camera );
     requestAnimationFrame( animate );
     stats.end();
